d133c6884b023269c38586e776e0cf0d2507a784
Finn Innes <finn123890@gmail.com>
2024-05-17 21:25:15 +1200

    created reactfullpage env

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,13 +1,41 @@
-import './styles/root.module.css'
+import ReactFullpage from '@fullpage/react-fullpage';
+import Scene from './components/Scene';
+import styles from './styles/root.module.css';
 
 const Root = () => {
 
   return (
-    <>
-      <div>
-        asd
-      </div>
-    </>
+    <div className={styles.main}>
+      <ReactFullpage
+        licenseKey={"gplv3-license"}
+        anchors={["home", "about", "work", "contact"]}
+        navigation // shows navigation circles
+        scrollingSpeed={700}
+        dragAndMove={"vertical"}
+        touchSensitivity={15}
+        keyboardScrolling={true}
+        credits={{ enabled: true }}
+        render={({  }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section">
+                <Scene />
+              </div>
+              <div className="section">
+                {/* <About /> */}
+              </div>
+              <div className="section">
+                {/* <Work /> */}
+              </div>
+              <div className="section">
+                {/* <Contact /> */}
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
+      {/* <Scene /> */}
+    </div>
   )
 }
 
