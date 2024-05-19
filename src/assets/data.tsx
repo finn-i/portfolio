87b3a9ad69019c0b1faee26c3743f06f42d5b832
Finn Innes <finn123890@gmail.com>
2024-05-19 22:24:33 +1200

    added data file to store textual information

---
+++ src/assets/data.tsx
@@ -0,0 +1,15 @@
+export const jobs = [
+  {
+    title: "Job Title",
+    description: "Job Desc",
+    img: "/b1.svg",
+  }
+]
+
+export const projects = [
+  {
+    title: "Project Title",
+    description: "Project Desc",
+    img: "/b1.svg",
+  }
+]
