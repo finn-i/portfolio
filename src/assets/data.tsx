2a51dedd00487e1905db0110da07330f352fa62e
Finn Innes <finn123890@gmail.com>
2024-05-20 22:57:12 +1200

    populated data contents

--- src/assets/data.tsx
+++ src/assets/data.tsx
@@ -1,15 +1,44 @@
-export const jobs = [
-  {
-    title: "Job Title",
-    description: "Job Desc",
-    img: "/b1.svg",
-  }
-]
-
 export const projects = [
   {
-    title: "Project Title",
-    description: "Project Desc",
-    img: "/b1.svg",
-  }
+    title: "Digital Library Developer",
+    description: "Software engineer expanding the Greenstone Digital Library Suite.",
+    github: "https://github.com/ateaspace/te-reo-autocomplete",
+    img: "./src/assets/gsdl.png",
+    stack: ["react", "python", "java", "perl"]
+  },
+  {
+    title: "Web Developer",
+    description: "Site maintenance for Newstead Country Preschool updating content and plugins.",
+    link: "https://newsteadcountrypreschool.co.nz",
+    img: "./src/assets/ncp.png",
+    stack: ["wordpress"]
+  },
+  {
+    title: "Fly Recipe Index",
+    description: "Allows fly tiers to explore a collection of patterns used for fly fishing.",
+    link: "https://flyrecipeindex.com",
+    img: "./src/assets/fri.png",
+    stack: ["react", "typescript", "mui", "aws"]
+  },
+  {
+    title: "NZ Outdoor Maps",
+    description: "Map viewer web app integrating data and layers from ARCGIS and DoC to visualise geographical features.",
+    link: "https://tramping-maps.vercel.app",
+    github: "https://github.com/finn-i/tramping-maps",
+    img: "./src/assets/doc.png",
+    stack: ["react", "mui", "leaflet", "arcgis"]
+  },
+  {
+    title: "MSc, Computer Science",
+    description: "Master of Science at The University of Waikato, investigating the retention of human edits in diarisation output combination.",
+    github: "https://github.com/finn-i/dover-lock",
+    img: "./src/assets/uow.png",
+    stack: ["LaTeX"]
+  },
+  {
+    title: "Diploma of Digital Photography",
+    description: "Developed practical and professional photography skills at The Southern Institute of Technology.",
+    img: "./src/assets/sit.png",
+    stack: ["adobe suite"]
+  },
 ]
