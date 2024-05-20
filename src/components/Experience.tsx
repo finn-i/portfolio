00deb2aa9475a7a93d2be91acef2a20e68db18b3
Finn Innes <finn123890@gmail.com>
2024-05-20 23:40:54 +1200

    cardsContainer is a motion.div for viewportIn animation

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -4,15 +4,26 @@ import { projects } from "../assets/data";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { motion } from 'framer-motion';
 
 const Experience = () => {
 
   const tiltOptions = {
     scale: 1.05,
+    max: 20,
   }
 
   return (
-    <div className={styles.cardContainer}>
+    <motion.div
+    className={styles.cardContainer}
+    initial="hidden"
+    whileInView="visible"
+    viewport={{ once: false }}
+    variants={{
+      visible: { opacity: 1, scale: 1, transition: { duration: 0.3 } },
+      hidden: { opacity: 0, scale: 0.5 }
+    }}
+    >
       {projects.map((project) => (
         <Tilt options={tiltOptions} className={styles.card}>
           <img src={project.img} alt={project.title} />
@@ -33,7 +44,7 @@ const Experience = () => {
           </div>
         </Tilt>
       ))}
-    </div>
+    </motion.div>
   );
 }
 
