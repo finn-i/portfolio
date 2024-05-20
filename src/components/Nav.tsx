86876479c31e75dfd1e06506d45f0f06edea86da
Finn Innes <finn123890@gmail.com>
2024-05-20 23:58:40 +1200

    basic nav setup

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,10 @@ import styles from '../styles/nav.module.css';
 const Nav = () => {
   return (
     <div className={styles.navContainer}>
-      asd
+      <a className={styles.navTitle} href={"#home"}>FINN</a>
+      <a className={styles.navItem} href={"#about"}>ABOUT</a>
+      <a className={styles.navItem} href={"#exp"}>PROJECTS</a>
+      <a className={styles.navItem} href={"#contact"}>CONTACT</a>
     </div>
   )
 }
