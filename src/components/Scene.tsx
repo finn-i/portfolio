2f98b2dd3296c36c8a1278fbc1363f4a030d3d9a
Finn Innes <finn123890@gmail.com>
2024-05-21 16:31:44 +1200

    adjusted rotation axis and point colors

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -2,9 +2,10 @@ import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import styles from '../styles/scene.module.css';
 import { useRef, useState } from 'react';
 import { Points, PointMaterial } from '@react-three/drei';
-import { inSphere } from 'maath/random';
+import { inSphere, inBox } from 'maath/random';
 import { Euler } from 'three';
 import  { useScroll } from 'framer-motion';
+import { theme } from '../assets/data';
 
 const Scene = () => {
   return (
@@ -18,12 +19,12 @@ const Scene = () => {
 
 const Stars = () =>  {
 
-  const ref = useRef(null);
-  const [sphere] = useState<any>(() => new Float32Array(inSphere(new Float64Array(5001), { radius: 0.2 })));
-  const [rotation, setRotation] = useState(new Euler(0, 0, 0));
+  const ref = useRef<any>(null);
+  const [sphere] = useState<any>(() => new Float32Array(inSphere(new Float64Array(3000), { radius: 0.125 })));
+  const [sphere2] = useState<any>(() => new Float32Array(inSphere(new Float64Array(3000), { radius: 0.2 })));
   const { scrollYProgress } = useScroll();
 
-  const ROTATION_DELTA = 0.0003;
+  const ROTATION_DELTA = 0.0004;
   const ROTATION_SCROLL_SPEED = 1.2;
   const CAMERA_X_OFFSET = 0.5;
   const CAMERA_Y_OFFSET = 0.5;
@@ -32,11 +33,12 @@ const Stars = () =>  {
     const scrollProgress = scrollYProgress.get();
     const rotationAngle = Math.PI * (scrollProgress * ROTATION_SCROLL_SPEED);
     
-    setRotation((prev) => new Euler(
-      rotationAngle + ROTATION_DELTA, 
-      rotationAngle + ROTATION_DELTA, 
-      prev.z  + ROTATION_DELTA
-    ));
+    ref.current.rotation.set(
+      rotationAngle + ROTATION_DELTA,
+      ref.current.rotation.y + ROTATION_DELTA, 
+      ref.current.rotation.z - ROTATION_DELTA, 
+      
+    );
 
     camera.position.x = 0 - scrollProgress * CAMERA_X_OFFSET;
     camera.position.y = 0 + scrollProgress * CAMERA_Y_OFFSET;
@@ -44,9 +46,12 @@ const Stars = () =>  {
   });
 
   return (
-    <group rotation={rotation}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} >
-        <PointMaterial transparent color={"rgb(0, 150, 250)"} size={0.002} sizeAttenuation={true} depthWrite={false} />
+    <group ref={ref}>
+      <Points positions={sphere} stride={3} frustumCulled={false} >
+        <PointMaterial transparent color={theme.accent} size={0.0015} sizeAttenuation={true} depthWrite={false} />
+      </Points>
+      <Points positions={sphere2} stride={3} frustumCulled={false} >
+        <PointMaterial transparent color={"rgb(30, 100, 30)"} size={0.002} sizeAttenuation={true} depthWrite={false} />
       </Points>
     </group>
   )
