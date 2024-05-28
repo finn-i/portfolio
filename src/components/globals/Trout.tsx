54cf77fc0c2e58a1c49abca70f4275015d82126a
Finn Innes <finn123890@gmail.com>
2024-05-28 17:54:32 +1200

    mobile fix for about page + removed trout hover

--- src/components/globals/Trout.tsx
+++ src/components/globals/Trout.tsx
@@ -1,13 +1,12 @@
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { useGLTF, useAnimations } from "@react-three/drei";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import styles from "../../styles/contact.module.css";
 
 const Model = () => {
   const group = useRef<any>(null);
   const { scene, animations } = useGLTF('scene.glb');
   const { actions } = useAnimations(animations, group);
-  const [hovered, setHovered] = useState(false)
 
   useEffect(() => {
     if (actions && actions.Trout_Swim) {
@@ -15,35 +14,10 @@ const Model = () => {
     }
   }, [actions]);
 
-  useEffect(() => {
-    document.body.style.cursor = hovered ? 'pointer' : 'auto';
-    if (hovered) group.current.scale.set(1.1, 1.1, 1.1);
-    else group.current.scale.set(1, 1, 1)
-  }, [hovered]);
-
-  useFrame(() => {
-    if (hovered) {
-      // group.current.rotation.set(group.current.rotation.x, group.current.rotation.y, group.current.rotation.z + 0.01)  
-    }
-  })
-
-  // useEffect(() => {
-  //   scene.traverse((node) => {
-  //     if ((node as Mesh).isMesh) {
-  //       ((node as Mesh).material as MeshBasicMaterial).wireframe = true;
-  //     }
-  //   });
-  // }, [scene]);
-
   return (
     <group 
       ref={group} 
       rotation={[0, 0, 0]} 
-      onPointerEnter={()=>{setHovered(true)}}
-      onPointerLeave={()=>{setHovered(false)}}
-      onClick={() => {
-        // rfpRef.current.fullpageApi.moveTo("home");
-      }}
     >
       <primitive object={scene} />
     </group>
@@ -57,7 +31,6 @@ const Trout = () => {
       <directionalLight position={[-2, 5, 0]} intensity={20} color={"rgb(20, 20, 255)"} />
       <directionalLight position={[-2, 5, 0]} intensity={9} />
       <Model />
-      {/* <OrbitControls enableDamping={false} /> */}
     </Canvas>
   );
 }
